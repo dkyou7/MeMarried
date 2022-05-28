c59d02382d17228b8f65c429f66a0ce76f5d49a2
youdongkwan <dkyou7@naver.com>
2022-05-28 10:06:30 +0900

    feat: MemoService 저장로직 구현

--- src/main/java/com/example/demo/service/MemoService.java
+++ src/main/java/com/example/demo/service/MemoService.java
@@ -1,12 +1,24 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Memo;
+import com.example.demo.repository.MemoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemoService {
 
+    @Autowired
+    private MemoRepository memoRepository;
+
     public String testService(){
-        return "test Service";
+        Memo memo = Memo.builder().content("안녕?").build();
+
+        memoRepository.save(memo);
+
+        // 바로 ID로 찾는게 가능하다.
+        Memo findMemo = memoRepository.findById(memo.getId()).get();
+        return findMemo.getContent();
     }
 
 }
