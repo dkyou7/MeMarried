dbceb316332fb56d4eb7cffabb322a7d72e9540b
youdongkwan <dkyou7@naver.com>
2022-05-28 10:44:38 +0900

    feat: MemoService create 함수 구현

--- src/main/java/com/example/demo/service/MemoService.java
+++ src/main/java/com/example/demo/service/MemoService.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
 @Service
 public class MemoService {
@@ -21,6 +23,30 @@ public class MemoService {
         // 바로 ID로 찾는게 가능하다.
         Memo findMemo = memoRepository.findById(memo.getId()).get();
         return findMemo.getContent();
+    }
+
+    public List<Memo> create(final Memo memo){
+        // 1. 검증
+        validate(memo);
+
+        // 2. save
+        memoRepository.save(memo);
+
+        log.info("Entity id : {} is saved.", memo.getId());
+
+        // 3. findAll
+        return memoRepository.findAll();
+    }
+
+    private void validate(Memo memo) {
+        if(memo == null){
+            log.warn("Entity cannot be null");
+            throw new RuntimeException("Entity cannot be null");
+        }
+        if(memo.getContent() == null){
+            log.warn("Unknown content.");
+            throw new RuntimeException("Unknown content.");
+        }
     }
 
 }
