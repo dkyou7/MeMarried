e217b8f6b52a498e9b2a54bd15b720abe914493c
youdongkwan <dkyou7@naver.com>
2022-05-28 10:07:15 +0900

    feat: MemoService에 로그를 남겨보자

--- src/main/java/com/example/demo/service/MemoService.java
+++ src/main/java/com/example/demo/service/MemoService.java
@@ -2,9 +2,11 @@ package com.example.demo.service;
 
 import com.example.demo.model.Memo;
 import com.example.demo.repository.MemoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class MemoService {
 
