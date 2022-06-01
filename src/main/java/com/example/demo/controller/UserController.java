c0a7be4eb9b39c2dc19a244f7cd47b4ea4626ee6
youdongkwan <dkyou7@naver.com>
2022-06-02 07:53:59 +0900

    feat: User SignUp 기능 작성

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,45 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.UserDTO;
+import com.example.demo.model.UserEntity;
+import com.example.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO){
+        try {
+            UserEntity user = UserEntity.builder()
+                    .email(userDTO.getEmail())
+                    .username(userDTO.getUsername())
+                    .password(userDTO.getPassword())
+                    .build();
+
+            UserEntity registerdUser = userService.create(user);
+            UserDTO responseUserDTO = userDTO.builder()
+                    .email(registerdUser.getEmail())
+                    .id(registerdUser.getId())
+                    .username(registerdUser.getUsername())
+                    .build();
+
+            return ResponseEntity.ok().body(responseUserDTO);
+        }catch (Exception e){
+            ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
 }
