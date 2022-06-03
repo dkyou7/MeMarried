c4a4ff9c598591c2974c91625ff4349053099052
You Dong Kwan <dkyou7@naver.com>
2022-06-03 14:20:17 +0900

    feat: 유저 password 암호화 저장

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -8,6 +8,8 @@ import com.example.demo.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +25,8 @@ public class UserController {
 
     @Autowired
     private TokenProvider tokenProvider;
+
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO){
@@ -51,7 +55,8 @@ public class UserController {
     public ResponseEntity<?> authenticate(@RequestBody UserDTO dto){
         UserEntity user = userService.getByCredentials(
                 dto.getEmail(),
-                dto.getPassword()
+                dto.getPassword(),
+                passwordEncoder
         );
         if(user != null){
             final String token = tokenProvider.create(user);
